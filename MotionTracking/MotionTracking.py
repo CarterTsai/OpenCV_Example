9379fb5d0ac8802a1b339def342428a961b76e24
Carter Tsai <hamming1@gmail.com>
2014-02-04 23:05:39 +0800

    Add searchMovement

--- MotionTracking/MotionTracking.py
+++ MotionTracking/MotionTracking.py
@@ -1,9 +1,40 @@
 #!/usr/bin/env python
 
+# https://dl.dropboxusercontent.com/u/28096936/tuts/motionTrackingTut/finalCode/motionTracking.cpp
 import cv2
 import sys
 
 sensitivity_value = 20
+count = 0
+theObject = [0,0]
+
+def searchMovement(thresholdImage, frame):
+    objectDetected = 0
+    largestContourVec = []
+    objectBoundingRectangle = []
+    x , y = 0 , 0
+
+    gray =  cv2.cvtColor(thresholdImage, cv2.COLOR_BGR2GRAY)
+
+    contours, hierarchy = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    if len(contours)>0 :
+        objectDetected = 1
+    else:
+        objectDetected = 0
+    if objectDetected:
+        largestContourVec.append(contours[len(contours) -1])
+        bx,by,bw,bh = cv2.boundingRect(largestContourVec[0])
+        px  = bx + bw/2
+        py  = by + bw/2
+        theObject[0] = px
+        theObject[0] = py
+
+    x = theObject[0]
+    y = theObject[1]
+    cv2.circle(frame, (x,y), 20 ,(0,255,0), 2)
+    return frame
+
 
 if __name__ == '__main__':
 
@@ -19,6 +50,7 @@ if __name__ == '__main__':
         sys.exit(-1)
 
     while(1):
+        count += 1
         ret, frame1 = cap.read()
         if not ret:
             break;
@@ -43,7 +75,10 @@ if __name__ == '__main__':
 
         ret, thresholdFrame = cv2.threshold(absFrame, sensitivity_value
                                        ,255, cv2.THRESH_BINARY)
-        cv2.imshow('thresholdFrame', thresholdFrame)
+
+
+        gy = searchMovement(thresholdFrame,frame1)
+        cv2.imshow('thresholdFrame', gy)
 
         key = cv2.waitKey(30)
         c = chr(key & 255)
